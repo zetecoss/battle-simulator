83caf95ff31dc0ca68e9c7802ea17aeb7cd65382
Your Name <you@example.com>
2026-03-12 17:18:50 +0000

    my first commit :)

---
+++ battlesim.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class creature;
+{
+    private:
+        int health ;
+        int  maxdamage;
+        string name ;
+    public:
+        creature(int health,int maxdamage,string name)
+        {
+            this->health = health;
+            this->maxdamage = maxdamage;
+            this->name = name;
+        }
+
+    
+
+
+
+
+
+};
