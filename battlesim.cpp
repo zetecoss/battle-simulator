fc3b4d2063c4cc053deac6c06a4df00090611eee
Your Name <you@example.com>
2026-03-13 20:35:28 +0000

    my first commit :)

--- battlesim.cpp
+++ battlesim.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-class creature;
+class creature
 {
     private:
         int health ;
@@ -17,10 +17,50 @@ class creature;
             this->name = name;
         }
 
-    
+        void takedamage(int damage_taken)
+        {
+            this->health = this->health - damage_taken;
+            cout << this->name << " took " << damage_taken << " pts of damage" << endl;
+            if(health <= 0)
+            {
+                health = 0;
+                cout << "you cant damage him more, hes dead" << endl;
+            }
+        }
+        int givedamage()
+        {
+            if(health <= 0)
+            {
+                health = 0;
+                cout << "he cant give damage, hes dead" << endl;
+                return 0;
+            }
+            
 
-
+            return this->maxdamage;
+        }
+        void showstats()
+        {
+            cout << this->name << " health:  " << this->health  << endl;
+        }
 
 
 
 };
+int main()
+{
+    creature x(1000,50,"monster");
+    creature y(40,50,"zombie");
+
+    x.showstats();
+    y.showstats();
+
+    x.takedamage(y.givedamage());
+    y.takedamage(x.givedamage());
+    x.takedamage(y.givedamage());
+
+    x.showstats();
+    y.showstats();
+
+    return 0;
+}
